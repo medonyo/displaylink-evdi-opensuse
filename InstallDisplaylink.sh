754553c95f78d7012f58649828d0a818cc569261
noone <no@noone.co>
2020-07-11 10:49:30 +0200

    Tumbleweed support for DisplayLink. Just work - not beauty. Feel free to change. TODO: 1. zypper has to resolve dependencies as apt is doing. For now it is stubbed. 2. code: -->>> dkms install \$(ls -t <<<--  may not need \ before $ in add_runit_service() function. It haven't been checked since Tumbleweed uses systemd.

--- InstallDisplaylink.sh
+++ InstallDisplaylink.sh
@@ -2,15 +2,49 @@
 
 WORKDIR="build"
 
-# Download the Ubuntu Driver from the DisplayLink webiste
-wget -O ${WORKDIR}/DisplayLink_Ubuntu_1.1.62.zip http://www.displaylink.com/downloads/file?id=607
+# code copied from https://github.com/AdnanHodzic/displaylink-debian/blob/master/displaylink-debian.sh
+########################################################################################dw
+# define the version to get as the latest available version
+version=`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep "download-version" | head -n 1 | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+# define download url to be the correct version
+dlurl="https://www.displaylink.com/"`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep 'class="download-link"' | head -n 1 | perl -pe '($_)=/<a href="\/([^"]+)"[^>]+class="download-link"/'`
+driver_dir=$version
+
+download() {
+local dlfileid=$(echo $dlurl | perl -pe '($_)=/.+\?id=(\d+)/')
+default=y
+echo -en "\nPlease read the Software License Agreement available at: \n$dlurl\nDo you accept?: [Y/n]: "
+read ACCEPT
+ACCEPT=${ACCEPT:-$default}
+case $ACCEPT in
+		y*|Y*)
+				echo -e "\nDownloading DisplayLink Ubuntu driver:\n"
+				wget -O DisplayLink_Ubuntu_${version}.zip "--post-data=fileId=$dlfileid&accept_submit=Accept" $dlurl
+				# make sure file is downloaded before continuing
+				if [ $? -ne 0 ]
+				then
+					echo -e "\nUnable to download Displaylink driver\n"
+					exit
+				fi
+				;;
+		*)
+				echo "Can't download the driver without accepting the license agreement!"
+				exit 1
+				;;
+esac
+}
+#########################################################################################up
 
 # Unpack the driver
 cd ${WORKDIR}
-unzip DisplayLink_Ubuntu_1.1.62.zip
-chmod 755 displaylink-driver-1.1.62.run
-./displaylink-driver-1.1.62.run --noexec --keep
-cd displaylink-driver-1.1.62
+download
+
+unzip DisplayLink_Ubuntu_${version}.zip
+installer=`find *.run -type f -print0` 
+chmod 755 $installer
+./$installer --noexec --keep
+installer_dir=`echo $installer | rev | cut -f2- -d '.'| rev`
+cd $installer_dir
 
 # Patch the installer, so it can installer everything on openSUSE Tumbleweed
 patch -p0 < ../../displaylink-installer.sh.opensuse.patch
